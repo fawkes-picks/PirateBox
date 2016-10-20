2904d78c0884aae549b2007745ea6370a833aaff
Caleb Meeson <fawkespickshop@gmail.com>
2016-10-20 10:49:03 -0700

    Untested Changes
    Added STDOUT redirect to PirateBox-Log.txt which is saved in the directory you run the script in.
    
    Added optional "if" statement for starting and enabling "minidlna"

--- pirate-setup.sh
+++ pirate-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+exec > >(tee -i PirateBox-Log.txt)
+ 
 if [[ $UID != 0 ]]; then
     echo "Please run this script with sudo:"
     echo "sudo $0 $*"
@@ -20,6 +22,12 @@ sudo cp /etc/minidlna.conf /etc/minidlna.conf.bkp
 
 sudo cp /opt/piratebox/src/linux.example.minidlna.conf /etc/minidlna.conf
 
-sudo systemctl start minidlna
+echo -e "Would you like to start and enable "minidlna"?: "
+read answer
+if [[ $answer == yes ]]; then
+    sudo systemctl start minidlna
+    sudo systemctl enable minidlna
+    
+    else exit 1
+fi    
 
-sudo systemctl enable minidlna
